02a2ab10a8b4bf8bb4c36db9dfe058034c68b57e
Susel Jena <suseljena143@gmail.com>
2026-09-02 01:41:58 +0530

    Delete src directory

--- src/mocks/browser.ts
+++
@@ -1,6 +0,0 @@
-import { setupWorker } from 'msw/browser';
-import { handlers } from './handlers';
-
-// The service worker is only started in development.
-// In production (VITE_MOCK=false), this file is never imported.
-export const worker = setupWorker(...handlers);
